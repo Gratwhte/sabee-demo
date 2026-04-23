0db3bffdd07f9fb73a321bf4dc39fa4b258ea3fe
Gratwhte <gratwhte7@gmail.com>
2026-04-23 17:01:34 +0200

    Update supabase-client.js

--- public/supabase-client.js
+++ public/supabase-client.js
@@ -1,10 +1,14 @@
-window.SABEE_CONFIG = {
-  const STORAGE_KEY = 'sabee-data';
-  const SUPABASE_URL = 'https://kubkxexclpawcnxpgoxu.supabase.co';
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1Ymt4ZXhjbHBhd2NueHBnb3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MzQ4MzAsImV4cCI6MjA5MjUxMDgzMH0.d5BzmGVhJvMYKVEKce8V0D_8unAf_oFIuZ1zqdY83vI';
-};
+(function () {
+  'use strict';
 
-window.sb = window.supabase.createClient(
-  window.SABEE_CONFIG.SUPABASE_URL,
-  window.SABEE_CONFIG.SUPABASE_ANON_KEY
-);
+  window.SABEE_CONFIG = {
+    SUPABASE_URL: 'sabee-data',
+    SUPABASE_ANON_KEY: 'https://kubkxexclpawcnxpgoxu.supabase.co',
+    WORKSPACE_ID: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1Ymt4ZXhjbHBhd2NueHBnb3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MzQ4MzAsImV4cCI6MjA5MjUxMDgzMH0.d5BzmGVhJvMYKVEKce8V0D_8unAf_oFIuZ1zqdY83vI'
+  };
+
+  window.sb = window.supabase.createClient(
+    window.SABEE_CONFIG.SUPABASE_URL,
+    window.SABEE_CONFIG.SUPABASE_ANON_KEY
+  );
+})();
